5170543b9326c3fcf58510c6cda57a66a67192e4
Ryan Garner <Rgarner4@umd.edu>
2025-11-19 17:01:30 -0500

    RgarnerBranch Initial Commit

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ import argparse
 
 import config
 from example_analysis import ExampleAnalysis
+from wordCount_analysis import WordCountAnalysis
 
 
 def parse_args():
@@ -48,7 +49,7 @@ config.overwrite_from_args(args)
 if args.feature == 0:
     ExampleAnalysis().run()
 elif args.feature == 1:
-    pass # TODO call first analysis
+    WordCountAnalysis().run()
 elif args.feature == 2:
     pass # TODO call second analysis
 elif args.feature == 3:
